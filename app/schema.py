27f8388943786f8013bc8d6843c734607c5e17fa
postassiums <batataprogramadora@gmail.com>
2025-02-24 21:11:49 -0300

    :sparkles: feat: add shiroko rvc voice

--- app/schema.py
+++ app/schema.py
@@ -15,7 +15,7 @@ class Id(BaseModel):
 
 Now=Field(default_factory=lambda  :datetime.now(tz=timezone.utc))
 
-class Voice(BaseModel):
+class MinioItem(BaseModel):
     url: str
     expires_at : datetime
     
@@ -26,21 +26,39 @@ class Voice(BaseModel):
         self.url=new_url
         self.expires_at=new_expiration
     
+
+
+class Voice(BaseModel):
+    normal_tts: MinioItem
+    rvc_tts: MinioItem
+    
+    def rvc_expired(self):
+        return self.rvc_tts.has_expired()
+    
+    def update_normal_tts(self, new_tts: MinioItem):
+        self.normal_tts=new_tts
+    
+    def update_rvc(self,new_rvc : MinioItem):
+        self.rvc_tts=new_rvc
+
 class OptionalTimestamps():
     updated_at : t.Optional[datetime]=None
     created_at: t.Optional[datetime]=None
     
     
-class Conversation(BaseModel,OptionalTimestamps):
+class Conversation(BaseModel):
     role: t.Literal['user','developer','assistent']='user'
     content: str | list[str]
     voice: t.Optional[Voice]=None
     
-    
+    def update_voice(self,normal_tts: MinioItem,rvc_tts: MinioItem):
+        self.voice=Voice(normal_tts=normal_tts,rvc_tts=rvc_tts)
     
     
     def has_voice(self):
         return self.voice is not None
+    
+
     
     @classmethod
     def get_projected_fields(cls):
